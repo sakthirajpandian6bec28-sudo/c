c750fc8f6edc1bb94220d063f78d31087c69b7bd
sakthirajpandian6bec28-sudo <sakthirajpandian6_bec28@mepcoeng.ac.in>
2026-07-09 07:22:06 +0530

    student profile input and display

--- STUDENT_PROFILE.c
+++ STUDENT_PROFILE.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main()
+{
+    char name[50], college[50], department[50];
+    int age, semester;
+    float cgpa;
+
+    printf("Enter Name: ");
+    scanf("%s", name);
+
+    printf("Enter Age: ");
+    scanf("%d", &age);
+
+    printf("Enter College: ");
+    scanf("%s", college);
+
+    printf("Enter Department: ");
+    scanf("%s", department);
+
+    printf("Enter Semester: ");
+    scanf("%d", &semester);
+
+    printf("Enter CGPA: ");
+    scanf("%f", &cgpa);
+
+    printf("\nStudent Profile\n");
+    printf("Name       : %s\n", name);
+    printf("Age        : %d\n", age);
+    printf("College    : %s\n", college);
+    printf("Department : %s\n", department);
+    printf("Semester   : %d\n", semester);
+    printf("CGPA       : %.2f\n", cgpa);
+
+    return 0;
+}
